2546592f539d1b4e27a8a635a31e2c3b9ff1ec3a
eronzg <eiriogbe@gmail.com>
2020-11-11 17:06:28 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Eromosele to the World!'));
+app.get('/', (req, res) => res.send('For the second time, Hello Eromosele to the World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
